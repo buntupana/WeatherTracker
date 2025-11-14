c727d896c3afd26f335d674bb4f3da4bfebfccde
alvaro <panabuntu@gmail.com>
2025-11-14 12:20:38 +0100

    feat: Set up project dependencies and configurations
    Configured the project with a comprehensive set of dependencies for various features, including:
    - Kotlin and Coroutines
    - Compose UI, Navigation, and Lifecycle
    - Koin for dependency injection
    - Ktor for networking
    - Room for local database storage
    - Coil for image loading
    - Datastore for preferences
    - Paging
    - Testing libraries
    
    The `libs.versions.toml` file is now structured with versions, libraries, plugins, and bundles. Plugin aliases have been added to the root `build.gradle.kts`.
    
    The app's `build.gradle.kts` is updated to use these new version catalog aliases and bundles. Proguard rules for minification and resource shrinking have been enabled for release builds.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 android {
     namespace = "com.panabuntu.weathertracker"
     compileSdk {
-        version = release(36)
+        version = release(libs.versions.compile.sdk.get().toInt())
     }
 
     defaultConfig {
         applicationId = "com.panabuntu.weathertracker"
-        minSdk = 24
-        targetSdk = 36
+        minSdk = libs.versions.min.sdk.get().toInt()
+        targetSdk = libs.versions.target.sdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -22,7 +22,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -34,7 +35,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         compose = true
@@ -42,14 +43,18 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
+    implementation(libs.kotlinx.serialization.json)
+
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.activity.compose)
+
+    // Material
     implementation(libs.androidx.compose.material3)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
